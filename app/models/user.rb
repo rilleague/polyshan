db00fa8269c67454d4b1765ceb618743371e7e41
rilleague <aarima2626@gmail.com>
2022-06-29 18:31:01 +0900

    deviseの導入 + usersテーブルの作成 + Userモデル内でバリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, length: { maximum: 6 }
+    validates :nickname
+    validates :email, format:{ with: /\A\S+@\S+\.\S+\z/ }
+  end
+end
